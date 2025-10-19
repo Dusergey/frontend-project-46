64a9b41f19c4b282cc181a83822c3089769fe52a
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-19 22:13:08 +0300

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,9 +7,9 @@ import genDiff from '../index.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
-const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8')
+const readFile = filepath => fs.readFileSync(filepath, 'utf-8')
 
 const resultStylish = readFile(getFixturePath('stylish.txt'))
 const resultPlain = readFile(getFixturePath('plain.txt'))
