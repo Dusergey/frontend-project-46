0f1ce9bc375ee29f7ead393810fd96490400b46b
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-19 21:16:56 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import genDiff from '../src/index.js';  // ← исправьте эту строку
+import { program } from 'commander'
+import genDiff from '../src/index.js'
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.2')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const result = genDiff(filepath1, filepath2, program.opts().format);
-    console.log(result);
+    const result = genDiff(filepath1, filepath2, program.opts().format)
+    console.log(result)
   })
-  .option('-f, --format <type>', 'output format', 'stylish');
-program.parse();
+  .option('-f, --format <type>', 'output format', 'stylish')
+program.parse()
