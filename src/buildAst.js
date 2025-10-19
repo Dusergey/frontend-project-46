82a94bf4143fd7afa26f825524b8b8ec3f3bfe30
Dusergey <129763975+Dusergey@users.noreply.github.com>
2025-10-19 21:24:01 +0300

    Update buildAst.js

--- src/buildAst.js
+++ src/buildAst.js
@@ -1,21 +1,21 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 const buildAst = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+  const keys1 = Object.keys(data1)
+  const keys2 = Object.keys(data2)
+  const keys = _.sortBy(_.union(keys1, keys2))
   const resultArr = keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+    const value1 = data1[key]
+    const value2 = data2[key]
     if (!_.has(data1, key)) {
-      return { key, value: value2, controlChanged: 'added' };
+      return { key, value: value2, controlChanged: 'added' }
     }
     if (!_.has(data2, key)) {
-      return { key, value: value1, controlChanged: 'removed' };
+      return { key, value: value1, controlChanged: 'removed' }
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      const children = buildAst(value1, value2);
-      return { key, children, controlChanged: 'nested' };
+      const children = buildAst(value1, value2)
+      return { key, children, controlChanged: 'nested' }
     }
     if (value1 !== value2) {
       return {
@@ -23,11 +23,11 @@ const buildAst = (data1, data2) => {
         value1,
         value2,
         controlChanged: 'changed',
-      };
+      }
     }
-    return { key, value: value1, controlChanged: 'unchanged' };
-  });
-  return resultArr;
-};
+    return { key, value: value1, controlChanged: 'unchanged' }
+  })
+  return resultArr
+}
 
-export default buildAst;
+export default buildAst
